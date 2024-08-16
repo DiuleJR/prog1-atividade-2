8cfe2845f9cd1c50bf73bbc24f8441b94746e1c9
DiuleJR <syfras999@gmail.com>
2024-08-16 03:15:12 -0300

    questão 11 resolvida 2.0

--- q11.c
+++ q11.c
@@ -14,37 +14,24 @@ int main() {
     scanf("%d %d %d", &ladoA, &ladoB, &ladoC);
 
     // Verificar se é um triângulo.
-    if (ladoA + ladoB > ladoC) {
-        if (ladoA + ladoC > ladoB) {
-            if (ladoB + ladoC > ladoA) {
-                
-                // Verifica se o triângulo é equilátero.
-                if (ladoA == ladoB && ladoB == ladoC) {
-                    printf("\nÉ um triângulo Equilátero!");
-                }
-
-                // Verifica se o triângulo é Isósceles.
-                else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
-                    printf("\nÉ um triângulo Isósceles");
-                }
-
-                // Verifica se o triângulo é Escaleno.
-                else {
-                    printf("\nÉ um triângulo Escaleno.");
-                }
-
-
-            }
-            else {
-                printf("Isso não é um triângulo!");
-            }
+    if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA) {
+        
+        // Verifica se o triângulo é equilátero.
+        if (ladoA == ladoB && ladoB == ladoC) {
+            printf("\nÉ um triângulo Equilátero!");
         }
+
+        // Verifica se o triângulo é Isósceles.
+        else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+            printf("\nÉ um triângulo Isósceles");
+        }
+
+        // Verifica se o triângulo é Escaleno.
         else {
-            printf("Isso não é um triângulo!");
+            printf("\nÉ um triângulo Escaleno.");
         }
-    }
 
-    else {
+    } else {
         printf("Isso não é um triângulo!");
     }
 
